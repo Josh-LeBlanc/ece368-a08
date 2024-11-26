0ebb3ee3afb674f28eb0328126f351a7a0b32a7e
Joshua Peter LeBlanc <jleblan@eceprog3.ecn.purdue.edu>
2024-11-25 21:39:42 -0500

    alright we have made progress

--- main.c
+++ main.c
@@ -27,7 +27,7 @@ Graph *get_inputs(char **, int *, int *);
 Node *create_node(int);
 Edge *create_edge(int, int);
 void print_graph(Graph *, int);
-void dijkstra(int, int, int, int *, Graph *);
+HeapNode* dijkstra(int, int, int, int *, Graph *);
 void dequeue(HeapNode *, int, int *);
 void update(HeapNode *, int, int *);
 int get_edge_num();
@@ -39,8 +39,6 @@ int main(int argc, char **argv) {
   int nw = 0; // num weights per edge
 
   Graph *g = get_inputs(argv, &nv, &nw);
-  print_graph(g, nw);
-  exit(1);
   int *heap_index = (int *)malloc(sizeof(int) * nv);
   // print_graph(g, nw);
   int source, dest;
@@ -49,129 +47,16 @@ int main(int argc, char **argv) {
     fprintf(stderr, "error, please enter source and destination in the format 'source dest':");
     exit(1);
   }
-  dijkstra(source, nv, nw, heap_index, g);
+  HeapNode* heap = dijkstra(source, nv, nw, heap_index, g);
 
-  printf("print heap index:\n\n");
+  printf("print distances:\n\n");
   for (int i = 0; i < nv; i++) {
-    printf("%d\n", heap_index[i]);
+    printf("%d\n", heap[i].distance);
   }
 
   assert(g == NULL); // free the graph and all nodes
   return 0;
 }
-
-/*
-Graph *get_inputs(char **argv, int *nv, int *nw) {
-  // open file
-  FILE *f = fopen(argv[1], "r");
-  if (!f) {
-    fprintf(stderr, "fopen in get_inputs failed");
-    exit(1);
-  }
-  // read num vertex and edge
-  if (fscanf(f, "%d %d\n", nv, nw) != 2) {
-    fprintf(stderr, "scanning first line of file failed");
-    exit(1);
-  }
-  // create graph
-  Graph *g = (Graph *)malloc(sizeof(Graph));
-  g->nodes = (Node **)malloc(sizeof(Node) * *nv);
-  g->num_nodes = 0;
-  // first node
-  int t;
-  Node *n;
-  Edge *e;
-  // read id
-  if (fscanf(f, "%d ", &t) != 1) {
-    fprintf(stderr, "first node id read failed");
-    exit(1);
-  }
-  n = create_node(t);
-  n->num_edges = 1;
-  // read edge
-  if (fscanf(f, "%d ", &t) != 1) {
-    fprintf(stderr, "first edge dest read failed");
-    exit(1);
-  }
-  e = create_edge(t, *nw);
-  // read weights
-  for (int i = 0; i < *nw - 1; i++) {
-    if (fscanf(f, "%d ", &t) != 1) {
-      fprintf(stderr, "weight reading failed");
-      exit(1);
-    }
-    e->weights[i] = t;
-  }
-  if (fscanf(f, "%d\n", &t) != 1) {
-    fprintf(stderr, "last weight reading failed");
-    exit(1);
-  }
-  e->weights[*nw - 1] = t;
-  n->edges[n->num_edges - 1] = e;
-  g->nodes[g->num_nodes++] = n;
-
-  // read rest of nodes
-  while (1) {
-    // check for EOF
-    if (feof(f)) {
-      break;
-    }
-    // id
-    if (fscanf(f, "%d ", &t) != 1) {
-      fprintf(stderr, "rest node id read failed");
-      exit(1);
-    }
-    // check if same node
-    if (t != g->nodes[g->num_nodes - 1]->id) {
-      // create new node
-      n = create_node(t);
-      n->num_edges = 1;
-      g->num_nodes++;
-      Node **temp = realloc(g->nodes, sizeof(Node *) * g->num_nodes);
-      if (temp == NULL) {
-        fprintf(stderr, "realloc node failed");
-        free(g);
-        exit(1);
-      }
-      g->nodes = temp;
-      g->nodes[g->num_nodes - 1] = n;
-    } else {
-      g->nodes[g->num_nodes - 1]->num_edges++;
-      Edge **temp =
-          realloc(g->nodes[g->num_nodes - 1]->edges,
-                  sizeof(Edge *) * g->nodes[g->num_nodes - 1]->num_edges);
-      if (temp == NULL) {
-        fprintf(stderr, "realloc edge failed");
-        free(g);
-        exit(1);
-      }
-      g->nodes[g->num_nodes - 1]->edges = temp;
-    }
-    if (fscanf(f, "%d ", &t) != 1) {
-      fprintf(stderr, "additional edge breaking");
-      exit(1);
-    }
-    e = create_edge(t, *nw);
-    for (int j = 0; j < *nw - 1; j++) {
-      if (fscanf(f, "%d ", &t) != 1) {
-        fprintf(stderr, "weight reading failed");
-        exit(1);
-      }
-      e->weights[j] = t;
-    }
-    if (fscanf(f, "%d\n", &t) != 1) {
-      fprintf(stderr, "last weight reading failed");
-      exit(1);
-    }
-    e->weights[*nw - 1] = t;
-    g->nodes[g->num_nodes - 1]
-        ->edges[g->nodes[g->num_nodes - 1]->num_edges - 1] = e;
-  }
-
-  fclose(f);
-  return g;
-}
-*/
 
 Graph *get_inputs(char **argv, int *nv, int *nw) {
   // open file
@@ -221,6 +106,7 @@ Graph *get_inputs(char **argv, int *nv, int *nw) {
       g->nodes[t]->edges = temp;
     }
     e = create_edge(t, *nw);
+    e->dest = t1;
     // read weights
     for (int i = 0; i < *nw - 1; i++) {
       if (fscanf(f, "%d ", &t2) != 1) {
@@ -259,7 +145,7 @@ Edge *create_edge(int dest, int nw) {
 
 void print_graph(Graph *g, int nw) {
   for (int i = 0; i < g->num_nodes; i++) {
-    printf("node #%d\n", g->nodes[i]->id);
+    printf("node #%d, num_edges: %d\n", g->nodes[i]->id, g->nodes[i]->num_edges);
     for (int j = 0; j < g->nodes[i]->num_edges; j++) {
       printf("edge #%d, dest %d\n", j, g->nodes[i]->edges[j]->dest);
       for (int k = 0; k < nw; k++) {
@@ -270,7 +156,7 @@ void print_graph(Graph *g, int nw) {
   }
 }
 
-void dijkstra(int source, int nv, int nw, int *heap_index, Graph *g) {
+HeapNode* dijkstra(int source, int nv, int nw, int *heap_index, Graph *g) {
   // taken from lecture slides
   HeapNode *heap = (HeapNode *)malloc(sizeof(HeapNode) * nv);
   int n = nv;
@@ -323,6 +209,7 @@ void dijkstra(int source, int nv, int nw, int *heap_index, Graph *g) {
       }
     }
   }
+  return heap;
 }
 
 void dequeue(HeapNode *heap, int n, int* heap_index) {
